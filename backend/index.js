24f48f9dbef110fa7daf1cf3f9628d9f30b3e05e
Pedro <pedroardo10@gmail.com>
2025-09-10 09:38:01 +0700

    fix the cors and origin

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://pern-todo-frontend-xi.vercel.app/",
+  })
+);
 app.use(express.json());
 
 app.use("/todos", todoRoutes);
